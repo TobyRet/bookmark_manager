da8a7dbfda011450157674cd058a1440aa5ecf6c
Toby Retallick <toby.retallick@gmail.com>
2014-03-24 13:35:51 +0000

    Home page displays link

--- server.rb
+++ server.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'data_mapper'
-
-env = ENV["RACK_ENV"] || "development"
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+env = ENV["RACK_ENV"] || "development" # defaulting to devopment
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}") # select database for enviro
 require './lib/link'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 get '/' do 
+  @links = Link.all
   erb :index
 end
 
