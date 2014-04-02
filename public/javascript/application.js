161e68971043c1142ebfecfbff4779111af4e90f
Toby Retallick <toby.retallick@gmail.com>
2014-04-02 15:29:47 +0100

    Created favourite button with jQuery

---
+++ public/javascript/application.js
@@ -0,0 +1,13 @@
+function addFavouritesHandler() {
+	$(".heart.solid").click(function(event) {
+		var link = $(this).parent();
+		var favourited = !!$(link).data("favourited");
+		var newOpacity = favourited ? 0 : 1;
+		$(link).data("favourited", !favourited);
+		$(this).animate({opacity: newOpacity}, 1000);
+	});
+}
+
+$(function() {
+	addFavouritesHandler();
+})
